04dddc0b8207dfacb6e9b124cb17d0282e1d59c5
sssadw <anthony.worrall@reading.ac.uk>
2022-06-15 15:29:20 +0100

    added generic scripts

---
+++ manifests/init.pp
@@ -0,0 +1,53 @@
+#
+class ngt_scripts (
+  String $log_dir      = '/var/log/ngt',
+  String $script_dir   = '/usr/local/sbin',
+) {
+
+  exec { "Create log dir ${log_dir}":
+    creates => $log_dir,
+    command => "mkdir -p ${log_dir}",
+    path => $::path,
+    before => File[$log_dir],
+  }
+  ensure_resource('file', $log_dir, { 'ensure' => 'directory'})
+
+  exec { "Create script dir ${script_dir}":
+    creates => $script_dir,
+    command => "mkdir -p ${script_dir}",
+    path => $::path,
+    before => File[$script_dir],
+  }
+  ensure_resource('file', $script_dir, { 'ensure' => 'directory'})
+
+  #NGT expects pre_freeze and post_that scripts in /usr/local/sbin
+  $ngt_sbin = '/usr/local/sbin'
+  exec { "Create NGT sbin ${ngt_sbin}":
+    creates => $ngt_sbin,
+    command => "mkdir -p ${ngt_sbin}",
+    path => $::path,
+    before => File[$ngt_sbin],
+  }
+  ensure_resource('file', $ngt_sbin, { 'ensure' => 'directory'})
+
+  file { "${ngt_sbin}/pre_freeze":
+    ensure => 'file',
+    content => epp('ngt_scripts/pre_freeze.epp', {
+       'log_dir' => $log_dir,
+       'script_dir' => $script_dir,
+     }),
+     owner => 'root',
+     mode  => '0700',
+  }
+
+  file { "${ngt_sbin}/post_thaw":
+    ensure => 'file',
+    content => epp('ngt_scripts/post_thaw.epp', {
+       'log_dir' => $log_dir,
+       'script_dir' => $script_dir,
+     }),
+     owner => 'root',
+     mode  => '0700',
+  }
+
+}
